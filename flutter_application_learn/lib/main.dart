d44911afc60fae742c2b8ae8997dc7b23f2dadf3
Gasheezy <mungai.gachango@gmail.com>
2021-03-06 11:53:41 +0300

    finish codelabs1

--- flutter_application_learn/lib/main.dart
+++ flutter_application_learn/lib/main.dart
@@ -9,28 +9,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       home: RandomWords(),
-      // body: Center(
-      //   child: RandomWords(),
-      // ),
     );
   }
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  Widget build(BuildContext context) {
-    //final wordPair = WordPair.random();
-    //return Text(wordPair.asPascalCase);
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  final List<WordPair> _suggestions = <WordPair>[];
-  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
@@ -40,19 +26,19 @@ class _RandomWordsState extends State<RandomWords> {
         // the word pairing. For odd rows, the function adds a
         // Divider widget to visually separate the entries. Note that
         // the divider may be difficult to see on smaller devices.
-        itemBuilder: (BuildContext _context, int i) {
-          // Add a one-pixel-high divider widget before each row
-          // in the ListView.
-          if (i.isOdd) {
-            return Divider();
-          }
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
 
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
           // The syntax "i ~/ 2" divides i by 2 and returns an
           // integer result.
           // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
           // This calculates the actual number of word pairings
           // in the ListView,minus the divider widgets.
-          final int index = i ~/ 2;
         });
   }
 
@@ -62,6 +48,16 @@ class _RandomWordsState extends State<RandomWords> {
         pair.asPascalCase,
         style: _biggerFont,
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
     );
   }
 }
